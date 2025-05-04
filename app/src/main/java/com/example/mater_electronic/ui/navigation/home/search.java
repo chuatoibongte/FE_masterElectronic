4511dfa299c3bb34a1472e949359b3bd54aa248b
chuatebongtoi <chitrung.tn@gmail.com>
2025-05-04 22:08:16 +0700

    create ui package and navigation

---
+++ app/src/main/java/com/example/mater_electronic/ui/navigation/home/search.java
@@ -0,0 +1,19 @@
+package com.example.mater_electronic.ui.navigation.home;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mater_electronic.R;
+
+public class search extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_search);
+
+    }
+}
