3eb07ab78241d63285983a243d8f697ff442605f
DatHuynhCoder <htdat2004@gmail.com>
2025-05-06 16:07:54 +0700

    Test trang register

--- app/src/main/java/com/example/mater_electronic/ui/register/Register.java
+++ app/src/main/java/com/example/mater_electronic/ui/register/Register.java
@@ -1,6 +1,7 @@
 package com.example.mater_electronic.ui.register;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +10,37 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.mater_electronic.R;
+import com.example.mater_electronic.databinding.ActivityRegisterBinding;
 
 public class Register extends AppCompatActivity {
+
+    private ActivityRegisterBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
+        //View Binding
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding.signupButton.setOnClickListener(v -> {
+            String username = binding.usernameInput.getText().toString();
+            String email = binding.emailInput.getText().toString();
+            String phone = binding.phoneInput.getText().toString();
+            String password = binding.passwordInput.getText().toString();
+
+            // Add your registration logic here
+            Log.e("Register", "Username: " + username + ", Email: " + email + ", Phone: " + phone + ", Password: " + password);
+        });
+
     }
 }
