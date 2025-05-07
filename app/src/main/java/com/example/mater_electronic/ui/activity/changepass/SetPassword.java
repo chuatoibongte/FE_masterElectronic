429fcd0c6c83cb8ff5afe7ae883529614fb588c4
le nguyen quynh anh . <huynhanh.170504@gmail.com>
2025-05-07 22:50:26 +0700

    reset password

---
+++ app/src/main/java/com/example/mater_electronic/ui/activity/changepass/SetPassword.java
@@ -0,0 +1,28 @@
+package com.example.mater_electronic.ui.activity.changepass;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.mater_electronic.R;
+import com.example.mater_electronic.databinding.ActivitySetPasswordBinding;
+
+public class SetPassword extends AppCompatActivity {
+
+    private ActivitySetPasswordBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        binding = ActivitySetPasswordBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+    }
+}
