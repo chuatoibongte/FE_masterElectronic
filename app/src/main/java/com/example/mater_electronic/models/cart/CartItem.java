3d6556ebfea718dfa09cba284527f7f64f928380
chuatebongtoi <chitrung.tn@gmail.com>
2025-05-13 15:54:22 +0700

    them file bottom_sheet_address.xml, sua file cartItem thêm vào parcelable, sửa file checkout, hoanh thanh checkout chua them luong xu ly khuc dạt hàng thành công hay không

--- app/src/main/java/com/example/mater_electronic/models/cart/CartItem.java
+++ app/src/main/java/com/example/mater_electronic/models/cart/CartItem.java
@@ -3,64 +3,64 @@ package com.example.mater_electronic.models.cart;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-public class CartItem {
-    private String img;
-    private String name;
-    private double price;
-    private int quantity;
-    private String mainCategory;
+public class CartItem implements Parcelable {
+        private String img;
+        private String name;
+        private double price;
+        private int quantity;
+        private String mainCategory;
 
     public CartItem(String img, String name, double price, int quantity, String mainCategory) {
-        this.img = img;
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
-        this.mainCategory = mainCategory;
-    }
+            this.img = img;
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+            this.mainCategory = mainCategory;
+        }
 
     public CartItem(String name, double price, int quantity) {
-        this.img = "";
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
-        this.mainCategory = "";
+            this.img = "";
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+            this.mainCategory = "";
+        }
+
+    // ✅ Bổ sung constructor để đọc từ Parcel
+    protected CartItem(Parcel in) {
+        img = in.readString();
+        name = in.readString();
+        price = in.readDouble();
+        quantity = in.readInt();
+        mainCategory = in.readString();
     }
 
-//    // ✅ Bổ sung constructor để đọc từ Parcel
-//    protected CartItem(Parcel in) {
-//        img = in.readString();
-//        name = in.readString();
-//        price = in.readDouble();
-//        quantity = in.readInt();
-//        mainCategory = in.readString();
-//    }
-//
-//    // ✅ Triển khai Parcelable
-//    public static final Parcelable.Creator<CartItem> CREATOR = new Parcelable.Creator<CartItem>() {
-//        @Override
-//        public CartItem createFromParcel(Parcel in) {
-//            return new CartItem(in);
-//        }
-//
-//        @Override
-//        public CartItem[] newArray(int size) {
-//            return new CartItem[size];
-//        }
-//    };
-//
-//
-//    public void writeToParcel(Parcel dest, int flags) {
-//        dest.writeString(img);
-//        dest.writeString(name);
-//        dest.writeDouble(price);
-//        dest.writeInt(quantity);
-//        dest.writeString(mainCategory);
-//    }
-//
-//
-//    public int describeContents() {
-//        return 0;
-//    }
+    // ✅ Triển khai Parcelable
+    public static final Parcelable.Creator<CartItem> CREATOR = new Parcelable.Creator<CartItem>() {
+       @Override
+        public CartItem createFromParcel(Parcel in) {
+            return new CartItem(in);
+        }
+
+       @Override
+        public CartItem[] newArray(int size) {
+            return new CartItem[size];
+       }
+    };
+
+
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(img);
+        dest.writeString(name);
+        dest.writeDouble(price);
+        dest.writeInt(quantity);
+        dest.writeString(mainCategory);
+    }
+
+
+    public int describeContents() {
+    return 0;
+   }
 
     public String getImg() {
         return img;
