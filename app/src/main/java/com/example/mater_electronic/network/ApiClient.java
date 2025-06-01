29732528d77a20bc421a963f499e5a4100f5719c
le nguyen quynh anh . <huynhanh.170504@gmail.com>
2025-06-02 01:15:57 +0700

    update: throw BASE_URL to its natural form

--- app/src/main/java/com/example/mater_electronic/network/ApiClient.java
+++ app/src/main/java/com/example/mater_electronic/network/ApiClient.java
@@ -10,7 +10,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "https://3da5-113-161-91-223.ngrok-free.app/";
+    private static final String BASE_URL = "http://13.239.123.42/backend/";
     private static Retrofit retrofit;
 
     public static AuthService getApiService() {
